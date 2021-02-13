7d5d553756d8c0344f53eda12e65f207a5629352
rich wilk <richardwilkinson@live.com>
2021-02-13 13:25:04 -0500

    added new api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,38 +2,56 @@ module.exports = function (app) {
 const Workout = require("../models/workout.js");
 
 app.get("/api/workouts", (req, res) => {
-    Workout.find({})
+    Workout.find()
+    .then(data => {
+        res.json(data);
+        console.log(data);
+    })
+    .catch(err => {
+        res.json(err);
+    });
+});
+
+app.post("/api/workouts", (req, res) => {
+    Workout.create(req.body)
     .then((dbWorkout) => {
         res.json(dbWorkout);
-        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
+app.put("/api/workouts:id", ({body, params}, res) => {
+    console.log("PARAMS", params, body);
+
+    Workout.findOneAndUpdate(
+        { _id: req.params.id },
+        { push: { exercises: req.body}},
+        {new: true, runValidators: true}
+    )
+    .then((dbWorkout) => {
+        res.json(dbWorkout);
     })
     .catch((err) => {
         res.json(err);
     });
 });
 
-app.post("/api/workouts", ({ body }, res) => {
+app.post("/api/workout/range", function (req, res){
     Workout.create({})
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
+    .then(data => res.json(data))
+    .catch(err => {
+        res.json(err);
     })
-    .catch(({ message }) => {
-        console.log(message);
-    });
 });
 
-app.put("/api/workouts:id", ({params, body}, res) => {
-    console.log("PARAMS", params, body);
-
-    Workout.findOneAndUpdate(
-        { _id: params.id },
-        { $push: {excercises: body }},
-        { new: true }
-    )
-    .then((dbWorkout) => {
-        res.json(dbWorkout);
+app.get("/api/workout/range", (req, res) => {
+    Workout.find({})
+    .then(data => {
+        res.send(data);
     })
-    .catch((err) => {
+    .catch(err => {
         res.json(err);
     });
 });
